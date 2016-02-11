f1916499b296361fe02321f167c53673cfd63b91
Marc Vertido <mvertido@Marcs-MacBook-Pro.local>
2016-02-11 17:13:43 -0500

    WHY ARE YOU FAILING ME?!?!?!?!

--- searcher.js
+++ searcher.js
@@ -1,3 +1,15 @@
-var searchBing = function(terms) {
-    return {};
+var searchBing = function (terms) {
+    var results;
+    $.ajax({
+        url: "/sample.json",
+        success: function (data) {
+            console.log("Hi");
+            results = JSON.parse(data);
+
+        },
+        failure: function (error) {
+            console.log(error);
+        }
+    });
+    return results;
 };
--- tests.js
+++ tests.js
@@ -9,3 +9,10 @@ QUnit.test("searchBing returns empty collection when input is empty", function( 
     var expected = 0;
     assert.equal (actual, expected, "Search returned an empty collection!");
 });
+
+QUnit.test("searchBing returns sample data when input is test", function( assert ) {
+	var results = searchBing("test");
+
+    var actual = results["d"]["results"].length;
+    assert.equal (actual, 50, "Search returned the sample data");
+});
