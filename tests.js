b3a5899f24cd95b863c59c096759abaa83529e21
David Ihnen <dihnen@pillartechnology.com>
2016-02-11 16:25:36 -0500

    test pass, results has a length

--- tests.js
+++ tests.js
@@ -7,7 +7,12 @@ QUnit.test( "data is parsable", function( assert ) {
   assert.ok( data = JSON.parse($("#shorttestdata").text()) );
 });
 
+var data = JSON.parse($("#shorttestdata").text());
 QUnit.test( "data has a d element", function( assert ) {
-  var data = JSON.parse($("#shorttestdata").text());
   assert.ok( "d" in data );
+});
+
+QUnit.test( "data has a results list", function( assert ) {
+  alert(typeof data.d.results.length)
+  assert.ok( 0 < data.d.results.length );
 });
