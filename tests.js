d355fd773816cac65a83fb8e53123e908723592c
David Ihnen <dihnen@pillartechnology.com>
2016-02-11 16:22:44 -0500

    test pass, data exists and parsable

--- tests.js
+++ tests.js
@@ -2,3 +2,12 @@ QUnit.test( "hello test", function( assert ) {
   assert.ok( 1 == "1", "Passed!" );
 });
 
+QUnit.test( "data is parsable", function( assert ) {
+  var data;
+  assert.ok( data = JSON.parse($("#shorttestdata").text()) );
+});
+
+QUnit.test( "data has a d element", function( assert ) {
+  var data = JSON.parse($("#shorttestdata").text());
+  assert.ok( "d" in data );
+});
