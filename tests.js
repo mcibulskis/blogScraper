d0417046b1edbde3c176519c9fed876e9713c1a6
David Ihnen <dihnen@pillartechnology.com>
2016-02-11 16:31:11 -0500

    test pass, results all have Url data

--- tests.js
+++ tests.js
@@ -13,6 +13,12 @@ QUnit.test( "data has a d element", function( assert ) {
 });
 
 QUnit.test( "data has a results list", function( assert ) {
-  alert(typeof data.d.results.length)
   assert.ok( 0 < data.d.results.length );
+});
+
+QUnit.test( "results have Url keys", function( assert ) {
+  var u;
+  for (u in data.d.results) {
+    assert.ok( typeof data.d.results[u].Url != "undefined" );
+  }
 });
