0a4388eaa503a017b0c6e104b02ebb131cbd874c
David Ihnen <dihnen@pillartechnology.com>
2016-02-12 11:19:27 -0500

    can retrieve text from server

--- tests.js
+++ tests.js
@@ -1,13 +1,15 @@
+//var url = require('url');
+
 QUnit.test( "hello test", function( assert ) {
   assert.ok( 1 == "1", "Passed!" );
 });
 
 QUnit.test( "data is parsable", function( assert ) {
   var data;
-  assert.ok( data = JSON.parse($("#shorttestdata").text()) );
+  assert.ok( data = JSON.parse($("#testdata").text()) );
 });
-
 var data = JSON.parse($("#shorttestdata").text());
+
 QUnit.test( "data has a d element", function( assert ) {
   assert.ok( "d" in data );
 });
@@ -26,11 +28,18 @@ QUnit.test( "results have Url keys", function( assert ) {
 QUnit.test( "url can be retrieved", function( assert ) {
   var u;
   for (u in data.d.results) {
-    var done = assert.async();
-    $.ajax({
-      url: data.d.results[u].Url,
-      success: function () { assert.ok(1); done(); }, 
-      error:   function () { assert.ok(0); done(); },
-     });
+    (function() {
+      var done = assert.async()
+      retrievePage(data.d.results[u].Url, function (xmlhttpreq, error) { 
+         assert.ok(0 < xmlhttpreq.responseText.length); done();
+      } )
+    })();
   }
 });
+
+QUnit.test( "twitter name can be spotted", function( assert ) {
+   assert.ok(1);
+});
+
+
+
